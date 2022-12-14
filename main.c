32eca1f2c1ad04ebe2483c1f25754b437f692956
Malaika-R20 <120583832+Malaika-R20@users.noreply.github.com>
2022-12-14 20:42:56 +0500

    table ready hai

---
+++ main.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int number;
+ int counter;
+	
+	printf("enter any number for table:\n");
+	scanf("%d",& number);
+	
+	for (counter=1;counter<=10;counter++){
+		
+printf("%d * %d = %d\n",number,counter,number*counter);		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+	
+	
+	return 0;
+}
